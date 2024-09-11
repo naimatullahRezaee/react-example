cb8326a659cc10b2da7b18bfee80d0425c346a74
naimatullahRezaee <naimatullahrezaee@gmail.com>
2024-09-11 09:03:08 +0430

    array

---
+++ src/AllFiles.tsx
@@ -0,0 +1,83 @@
+// import { useState } from "react";
+// import Alert from "./components/Alert";
+// import Button from "./components/Button";
+// import ListGroup from "./components/ListGroup";
+// import Like from "./components/Like";
+// import Message from "./Message";
+// function App() {
+// const [alertVisibile, setAlertVisibile] = useState(false);
+// const items = ["Kabul", "Kandakhar", "Mazar"];
+
+// const [drink, setDrink] = useState({
+//   name: "ali",
+//   address: {
+//     city: "Kandakhar",
+//     zipcode: 100,
+//   },
+// });
+// const handleClick = () => {
+//   setDrink({ ...drink, address: { ...drink.address, zipcode: 101 } });
+// };
+
+// const [tags, setTags] = useState(["happy", "cheerful"]);
+
+// const handleClick = () => {
+// add new item to the list
+// setTags([...tags, "exciting"]);
+// delete an item from Array
+// setTags(tags.filter((tag) => tag !== "happy"));
+// update an item in the Array
+
+//   setTags(tags.map((tag) => (tag === "happy" ? "happiness" : tag)));
+// };
+
+//   const [bugs, setBugs] = useState([
+//     { id: 1, title: "ali", fixed: false },
+//     { id: 2, title: "karim", fixed: false },
+//   ]);
+
+//   const handleClick = () => {
+//     setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+//   };
+
+//   return (
+//     <div>
+//       <div>
+//         {" "}
+//         {bugs.map((bug) => (
+//           <p key={bug.id}>
+//             {" "}
+//             {bug.title} {bug.fixed ? "fixed" : "new"}{" "}
+//           </p>
+//         ))}{" "}
+//       </div>
+//       <button onClick={handleClick}> click </button>
+{
+  /* <h1>{drink.address.zipcode}</h1> */
+}
+
+{
+  /* <Like onClick={() => console.log("Click")} /> */
+}
+{
+  /* <ListGroup
+        items={items}
+        heading="List"
+        onSelectItem={() => console.log("")}
+      /> */
+}
+{
+  /* {alertVisibile && (
+        <Alert onClose={() => setAlertVisibile(false)}>
+          Hi dear how are you
+        </Alert>
+      )}
+      <Button onClick={() => setAlertVisibile(true)}>My Button</Button> */
+}
+{
+  /* </div>
+  );
+}
+
+export default App; */
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,7 @@
 import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
-import ListGroup from "./components/ListGroup";
-import Like from "./components/Like";
-import Message from "./Message";
-function App() {
-  // const [alertVisibile, setAlertVisibile] = useState(false);
-  // const items = ["Kabul", "Kandakhar", "Mazar"];
 
-  return (
-    <div>
-      {/* <Like onClick={() => console.log("Click")} /> */}
-      {/* <ListGroup
-        items={items}
-        heading="List"
-        onSelectItem={() => console.log("")}
-      /> */}
-      {/* {alertVisibile && (
-        <Alert onClose={() => setAlertVisibile(false)}>
-          Hi dear how are you
-        </Alert>
-      )}
-      <Button onClick={() => setAlertVisibile(true)}>My Button</Button> */}
-    </div>
-  );
+function App() {
+  return <div> Hello </div>;
 }
 
 export default App;
