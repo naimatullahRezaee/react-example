6018616f2277c47919163692aed2d34d69c67541
naimatullahRezaee <naimatullahrezaee@gmail.com>
2024-09-11 11:27:45 +0430

    excercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import { useState } from "react";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
+import ExpandableText from "./components/ExpandableText";
 function App() {
-  const [cartItem, setCartItem] = useState([
-    "product1",
-    "product2",
-    "product3",
-  ]);
   return (
     <div>
-      <NavBar cartItemCount={cartItem.length} />
-      <Cart cartItems={cartItem} onClear={() => setCartItem([])} />
+      <ExpandableText maxChars={50}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+        beatae suscipit quod reiciendis voluptatem, veniam, sed accusamus quia
+        nemo nesciunt facilis. Hic, vero excepturi eaque atque incidunt
+        repudiandae? Perferendis odio esse et obcaecati ad animi consequatur
+        rerum itaque quisquam explicabo facere distinctio natus id expedita
+        ipsam, dolorum dicta velit eos! Inventore rem quaerat cum temporibus
+        laborum accusantium veritatis tenetur ipsam. Tenetur culpa adipisci,
+        velit qui quis, at neque accusamus modi itaque numquam similique quae
+        illum, autem dignissimos. Porro, harum. Nesciunt sequi voluptates
+        consequuntur est asperiores tempore minima culpa libero modi facilis
+        quis illo, sunt, velit nemo. Dolore incidunt expedita consectetur!
+      </ExpandableText>
     </div>
   );
 }
---
+++ src/components/Excercise.tsx
@@ -0,0 +1,71 @@
+// import { useState } from "react";
+// import NavBar from "./components/NavBar";
+// import Cart from "./components/Cart";
+// function App() {
+// const [cartItem, setCartItem] = useState([
+//   "product1",
+//   "product2",
+//   "product3",
+// ]);
+
+// const [game, setGame] = useState({
+//   id: 1,
+//   player: {
+//     name: "Ali",
+//   },
+// });
+
+// const handleClick = () => {
+//   setGame({ ...game, player: { ...game.player, name: "karim" } });
+// };
+// const [pizza, setPizza] = useState({
+//   name: "Pizza",
+//   marterails: ["mashroom"],
+// });
+
+// const handleClick = () => {
+//   setPizza({ ...pizza, marterails: [...pizza.marterails, "Cheese"] });
+// };
+
+// const [cart, setCart] = useState({
+//   discount: 0.1,
+//   items: [
+//     { id: 1, title: "product1", quantity: 1 },
+//     { id: 2, title: "product2", quantity: 1 },
+//   ],
+// });
+
+// const handleClick = () => {
+//   setCart({
+//     ...cart,
+//     items: cart.items.map((item) =>
+//       item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+//     ),
+//   });
+// };
+//   return (
+//     <div>
+{
+  /* <button onClick={handleClick}>Click</button>
+      <h1>{cart.discount}</h1>
+      <h2>
+        {cart.items.map((item) => (
+          <ul>
+            <li>{item.id}</li>
+            <li>{item.title}</li>
+            <li>{item.quantity}</li>
+          </ul>
+        ))} */
+}
+{
+  /* </h2> */
+}
+{
+  /* <NavBar cartItemCount={cartItem.length} />
+      <Cart cartItems={cartItem} onClear={() => setCartItem([])} /> */
+}
+//     </div>
+//   );
+// }
+
+// export default App;
