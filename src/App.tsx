ce610bf9e64389042867f78952208fdd94262818
naimatullahRezaee <naimatullahrezaee@gmail.com>
2024-09-11 09:18:07 +0430

    NavBar item

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import { useState } from "react";
-
+import NavBar from "./components/NavBar";
+import Cart from "./components/Cart";
 function App() {
-  return <div> Hello </div>;
+  const [cartItem, setCartItem] = useState([
+    "product1",
+    "product2",
+    "product3",
+  ]);
+  return (
+    <div>
+      <NavBar cartItemCount={cartItem.length} />
+      <Cart cartItems={cartItem} onClear={() => setCartItem([])} />
+    </div>
+  );
 }
 
 export default App;
