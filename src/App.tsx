0f1d412f387e532afb3ef29fa5bb80f16e63d501
naimatullahRezaee <naimatullahrezaee@gmail.com>
2024-09-12 07:19:32 +0430

    form

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
 import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 function App() {
   return (
     <div>
-      <ExpandableText maxChars={50}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-        beatae suscipit quod reiciendis voluptatem, veniam, sed accusamus quia
-        nemo nesciunt facilis. Hic, vero excepturi eaque atque incidunt
-        repudiandae? Perferendis odio esse et obcaecati ad animi consequatur
-        rerum itaque quisquam explicabo facere distinctio natus id expedita
-        ipsam, dolorum dicta velit eos! Inventore rem quaerat cum temporibus
-        laborum accusantium veritatis tenetur ipsam. Tenetur culpa adipisci,
-        velit qui quis, at neque accusamus modi itaque numquam similique quae
-        illum, autem dignissimos. Porro, harum. Nesciunt sequi voluptates
-        consequuntur est asperiores tempore minima culpa libero modi facilis
-        quis illo, sunt, velit nemo. Dolore incidunt expedita consectetur!
-      </ExpandableText>
+      <Form />
     </div>
   );
 }
